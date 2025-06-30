5ce8d72140766c5a3f472598b981b738b31db909
RNanWP <renan92011@hotmail.com>
2025-06-30 14:10:24 -0300

    Feat: atualiznado as dependencias

--- Back-end/app.ts
+++ Back-end/app.ts
@@ -2,3 +2,12 @@
 //
 // reenan92011
 // uTUCcf8O3A2bB8P1
+
+
+
+// DICAS E APRENDIZADOS
+// 
+// .ENV usado para adicionar a rota do MongoDB. 
+// Ex:PORT=3000
+// MONGO_URI=mongodb://mongo:27017/blogdb
+// JWT_SECRET=seuSegredoAqui
